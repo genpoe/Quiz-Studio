d12d1a771bedeab3d95c7907a7b17ebfa37be2c8
Gil <marsh.skc@gmail.com>
2020-10-20 19:16:58 -0500

    Add project files.

---
+++ Quiz/Question.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Quiz
+{
+    public abstract class Question
+    {
+        string questionText;
+        Dictionary<string, double> answers;
+        double points;
+
+        public Question(string questionText, Dictionary<string, double> answers, double points)
+        {
+            this.questionText = questionText;
+            this.answers = answers;
+            this.points = points;
+        }
+    }
+}
